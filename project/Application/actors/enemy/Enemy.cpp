c0a0c53dc077a89587681e685e1a3d28b8302594
reachaso <reachaso040505@gmail.com>
2026-03-02 10:26:54 +0900

    一旦仮

--- project/Application/actors/enemy/Enemy.cpp
+++ project/Application/actors/enemy/Enemy.cpp
@@ -1,1 +1,34 @@
 #include "Enemy.h"
+#include "camera/Camera.h"
+
+Enemy::~Enemy() {}
+
+void Enemy::Initialize(Camera *camera) {
+
+  // ボディの初期化
+  body_ = std::make_unique<ObjClass>();
+  body_->Initialize(camera, "enemy_body.obj");
+  isActivebody_ = true;
+  // 頭の初期化
+  head_ = std::make_unique<ObjClass>();
+  head_->Initialize(camera, "enemy_head.obj");
+  isActivehead_ = true;
+}
+
+void Enemy::Update() {
+  if (isActivebody_) {
+    body_->Update();
+  }
+  if (isActivehead_) {
+    head_->Update();
+  }
+}
+
+void Enemy::Draw() {
+  if (isActivebody_) {
+    body_->Draw();
+  }
+  if (isActivehead_) {
+    head_->Draw();
+  }
+}
--- project/Application/actors/enemy/Enemy.h
+++ project/Application/actors/enemy/Enemy.h
@@ -1,5 +1,29 @@
 #pragma once
-class Enemy
-{
-};
+#include "3D/ObjClass.h"
+#include <GameApplication.cpp>
+#include <memory>
 
+class Camera;
+
+class Enemy {
+
+public:
+  ~Enemy();
+
+  void Initialize(Camera *camera);
+
+  void Update();
+  void Draw();
+
+private:
+  // エンジン
+  IrufemiEngine *engine_ = nullptr;
+
+  std::unique_ptr<Camera> camera_ = nullptr;
+
+  std::unique_ptr<ObjClass> body_ = nullptr;
+  bool isActivebody_ = false;
+
+  std::unique_ptr<ObjClass> head_ = nullptr;
+  bool isActivehead_ = false;
+};
