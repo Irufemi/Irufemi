606013216a97ffad68fe46ae0c8455235a854c7c
Koichi_Suehiro <k024g0001@g.neec.ac.jp>
2026-03-02 16:44:46 +0900

    SelectSceneの変更が抜けていたので追加

--- project/Application/scene/stageSelect/SelectScene.cpp
+++ project/Application/scene/stageSelect/SelectScene.cpp
@@ -87,9 +87,19 @@ void SelectScene::Update() {
 #endif // USE_IMGUI
 
     // --- カメラの更新 ---
-    // 現在アクティブなカメラへのポインタ
-    Camera* currentCamera = debugMode_ ? const_cast<Camera*>(&debugCamera_->GetCamera()) : camera_.get();
-    currentCamera->Update("Camera"); // デバッグカメラも通常カメラもUpdateを呼ぶ
+    if (debugMode_) {
+        // デバッグカメラを更新
+        debugCamera_->Update();
+        // デバッグカメラの計算結果をメインカメラに上書きする
+        const Camera& dbgCam = debugCamera_->GetCamera();
+        camera_->SetViewMatrix(dbgCam.GetViewMatrix());
+        camera_->SetTranslate(dbgCam.GetTranslate());
+        camera_->SetPerspectiveFovMatrix(dbgCam.GetPerspectiveFovMatrix());
+    }
+    else {
+        // 通常カメラの更新
+        camera_->Update("Camera");
+    }
 
     // =====
     // ↓ゲームの更新
@@ -108,9 +118,9 @@ void SelectScene::Update() {
 
     // --- フレーム共通データのセット ---
     CameraForGPU cameraForGpu;
-    cameraForGpu.view = currentCamera->GetViewMatrix();
-    cameraForGpu.projection = currentCamera->GetPerspectiveFovMatrix();
-    cameraForGpu.worldPosition = currentCamera->GetTranslate();
+    cameraForGpu.view = camera_->GetViewMatrix();
+    cameraForGpu.projection = camera_->GetPerspectiveFovMatrix();
+    cameraForGpu.worldPosition = camera_->GetTranslate();
 
     std::vector<PointLight*> pLights;
     for (const auto& light : pointLights_) {
