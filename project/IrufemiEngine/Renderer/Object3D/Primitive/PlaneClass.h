fc5b1683f0396955e45fe2104f70ceea0a59aeea
Koichi Suehiro <irufemi.1215@gmail.com>
2026-03-06 04:30:15 +0900

    ファイル整理 インクルードの整理はこれから

--- project/IrufemiEngine/Renderer/Object3D/Primitive/PlaneClass.h
+++ project/IrufemiEngine/Renderer/Object3D/Primitive/PlaneClass.h
@@ -57,6 +57,18 @@ public: //メンバ関数
     // Planeの情報を取得
     Plane GetInfo() const { return info_; }
 
+    void SetTransform(const Transform& transform) { resource_->transform_ = transform; }
+    Transform GetTransform()const { return resource_->transform_; }
+
+    void SetTranslate(const Vector3& translate) { resource_->transform_.translate = translate; }
+    Vector3 GetTranslate()const { return resource_->transform_.translate; }
+
+    void SetScale(const Vector3& scale) { resource_->transform_.scale = scale; }
+    Vector3 GetScale()const { return resource_->transform_.scale; }
+
+    void SetRotate(const Vector3& rotate) { resource_->transform_.rotate = rotate; }
+    Vector3 GetRotate()const { return resource_->transform_.rotate; }
+
     static void SetTextureManager(TextureManager* texM) { textureManager_ = texM; }
     static void SetDrawManager(DrawManager* drawM) { drawManager_ = drawM; }
     static void SetDebugUI(DebugUI* ui) { ui_ = ui; }
