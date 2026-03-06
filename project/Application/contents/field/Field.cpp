e1c4aceb29c44ce89bc59878d120333c9fe5839c
zuizui2525 <casa01732925@gmail.com>
2026-03-06 11:05:17 +0900

    マップを修正

--- project/Application/contents/field/Field.cpp
+++ project/Application/contents/field/Field.cpp
@@ -8,25 +8,61 @@
 Field::Field(Camera* camera, IrufemiEngine* engine) {
 	camera_ = camera;
 	engine_ = engine;
+	input_ = engine_->GetInputManager();
 }
 
 Field::~Field() {
 }
 
 void Field::Initialize() {
-	plane_ = std::make_unique<PlaneClass>();
-	plane_->Initialize(camera_);
-	plane_->SetScale({ 200.0f,200.0f,200.0f });
-	plane_->SetRotate({ 1.57f,0.0f, 0.0f });
-	plane_->SetTranslate({ 0.0f,0.0f,0.0f });
-	plane_->SetColor({ 0.0f,0.5f,0.0f,1.0f });
+	pFloor_ = std::make_unique<PlaneClass>();
+	pFloor_->Initialize(camera_);
+	pFloor_->SetScale({ 200.0f,200.0f,200.0f });
+	pFloor_->SetRotate({ 1.57f,0.0f, 0.0f });
+	pFloor_->SetTranslate({ 0.0f,0.0f,0.0f });
+	pFloor_->SetColor({ 0.0f,1.0f,0.0f,1.0f });
+
+	pPZWall_ = std::make_unique<PlaneClass>();
+	pPZWall_->Initialize(camera_);
+	pPZWall_->SetScale({ 200.0f,10.0f,200.0f });
+	pPZWall_->SetTranslate({ 0.0f,5.0f,100.0f });
+	pPZWall_->SetColor({ 0.0f,0.0f,1.0f,1.0f });
+
+	pMZWall_ = std::make_unique<PlaneClass>();
+	pMZWall_->Initialize(camera_);
+	pMZWall_->SetScale({ 200.0f,10.0f,200.0f });
+	pMZWall_->SetRotate({ 0.0f,3.14f, 0.0f });
+	pMZWall_->SetTranslate({ 0.0f,5.0f,-100.0f });
+	pMZWall_->SetColor({ 0.0f,0.0f,1.0f,1.0f });
+
+	pPXWall_ = std::make_unique<PlaneClass>();
+	pPXWall_->Initialize(camera_);
+	pPXWall_->SetScale({ 200.0f,10.0f,200.0f });
+	pPXWall_->SetRotate({ 0.0f,1.57f, 0.0f });
+	pPXWall_->SetTranslate({ 100.0f,5.0f,0.0f });
+	pPXWall_->SetColor({ 0.0f,0.0f,1.0f,1.0f });
+
+	pMXWall_ = std::make_unique<PlaneClass>();
+	pMXWall_->Initialize(camera_);
+	pMXWall_->SetScale({ 200.0f,10.0f,200.0f });
+	pMXWall_->SetRotate({ 0.0f,-1.57f, 0.0f });
+	pMXWall_->SetTranslate({ -100.0f,5.0f,0.0f });
+	pMXWall_->SetColor({ 0.0f,0.0f,1.0f,1.0f });
 }
 
 void Field::Update() {
-	plane_->Debug();
-	plane_->Update();
+	pFloor_->Debug();
+	pFloor_->Update();
+	pPZWall_->Update();
+	pMZWall_->Update();
+	pPXWall_->Update();
+	pMXWall_->Update();
 }
 
 void Field::Draw() {
-	plane_->Draw();
+	pFloor_->Draw();
+	pPZWall_->Draw();
+	pMZWall_->Draw();
+	pPXWall_->Draw();
+	pMXWall_->Draw();
 }
