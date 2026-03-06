b5862ac594645e18804877b65d0302a13f237ebd
zuizui2525 <casa01732925@gmail.com>
2026-03-06 10:08:13 +0900

    修正

--- project/Application/contents/field/Field.cpp
+++ project/Application/contents/field/Field.cpp
@@ -1,1 +1,27 @@
 #include "Field.h"
+
+#include "engine/Input/InputManager.h"
+#include "engine/IrufemiEngine.h"
+#include "camera/Camera.h"
+
+Field::Field(InputManager* input, Camera* camera, IrufemiEngine* engine) {
+	input_ = input;
+	camera_ = camera;
+	engine_ = engine;
+}
+
+Field::~Field() {
+}
+
+void Field::Initialize() {
+	plane_ = std::make_unique<PlaneClass>();
+	plane_->Initialize(camera_);
+}
+
+void Field::Update() {
+	plane_->Update();
+}
+
+void Field::Draw() {
+	plane_->Draw();
+}
--- project/Application/contents/field/Field.h
+++ project/Application/contents/field/Field.h
@@ -1,5 +1,26 @@
 #pragma once
-class Field
-{
+#include "3D/PlaneClass.h"
+#include "3D/ObjClass.h"
+
+class Camera;
+class InputManager;
+class IrufemiEngine;
+
+class Field{
+public:
+	Field(InputManager* input, Camera* camera, IrufemiEngine* engine);
+	~Field();
+	void Initialize();
+	void Update();
+	void Draw();
+private:
+	// 外部依存
+	InputManager* input_ = nullptr;
+	Camera* camera_ = nullptr;
+	IrufemiEngine* engine_ = nullptr;
+
+	// マップのモデル
+	std::unique_ptr<PlaneClass> plane_ = nullptr;
+	std::unique_ptr<ObjClass> map_ = nullptr;
 };
 
