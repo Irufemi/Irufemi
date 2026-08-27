ce29a45dd8bf22a5a1721c7a87a68ae4f5ecafff
Koichi Suehiro <k024g0001@g.neec.ac.jp>
2026-08-28 01:41:19 +0900

    【機能追加】PostProcessSettings の追加と各ポストプロセス設定のコンポーネント化

---
+++ project/IrufemiEditor/Inspectors/Effects/GlobalPostProcessComponentEditor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef EditorMode
+#include "Core/IComponentEditor.h"
+#include <string>
+
+class GlobalPostProcessComponent;
+
+/**
+ * @class GlobalPostProcessComponentEditor
+ * @brief GlobalPostProcessComponent の専用エディタ UI
+ */
+class GlobalPostProcessComponentEditor : public IComponentEditor {
+public:
+    GlobalPostProcessComponentEditor() = default;
+    ~GlobalPostProcessComponentEditor() override = default;
+
+    void Draw(Component* component, EditorActionManager* actionManager) override;
+
+private:
+    void DrawFloatProperty(const char* label, float& value, float defaultValue, float minVal, float maxVal, EditorActionManager* actionManager);
+    void DrawBoolProperty(const char* label, bool& value, bool defaultValue, EditorActionManager* actionManager);
+};
+
+#endif // EditorMode
--- project/IrufemiEngine/Framework/Component/Effect/GlobalPostProcessComponent.cpp
+++ project/IrufemiEngine/Framework/Component/Effect/GlobalPostProcessComponent.cpp
@@ -6,23 +6,8 @@
 #include "Renderer/System/Core/BaseModel.h"
 
 void GlobalPostProcessComponent::OnRegisterProperties() {
-    RegisterHeader("Bloom Settings");
-    RegisterProperty("Enable Bloom", &enableBloom_);
-    RegisterProperty("Bloom Threshold", &bloomThreshold_).SetMinMax(0.0f, 10.0f);
-    RegisterProperty("Bloom Intensity", &bloomIntensity_).SetMinMax(0.0f, 10.0f);
-    RegisterProperty("Bloom Sigma", &bloomSigma_).SetMinMax(0.1f, 20.0f);
-
-    RegisterHeader("Vignette Settings");
-    RegisterProperty("Enable Vignette", &enableVignette_);
-    RegisterProperty("Vignette Radius", &vignetteRadius_).SetMinMax(0.0f, 1.5f);
-    RegisterProperty("Vignette Softness", &vignetteSoftness_).SetMinMax(0.0f, 1.0f);
-
-    RegisterHeader("Color Grading (ToneMapping & HSV)");
-    RegisterProperty("Enable Color Grading", &enableColorGrading_);
-    RegisterProperty("Exposure", &exposure_).SetMinMax(0.1f, 5.0f);
-    RegisterProperty("Hue", &hue_).SetMinMax(-180.0f, 180.0f);
-    RegisterProperty("Saturation", &saturation_).SetMinMax(-1.0f, 2.0f);
-    RegisterProperty("Value", &value_).SetMinMax(-1.0f, 2.0f);
+    // GUIはGlobalPostProcessComponentEditorによって描画されるため、
+    // RegisterPropertyは使用しない。
 }
 
 void GlobalPostProcessComponent::Start() {
@@ -41,67 +26,58 @@ void GlobalPostProcessComponent::Update() {
     auto pp = engine->GetPostProcessManager();
     if (!pp) return;
 
-    // 1. Bloom - 空間・ぼかし系
-    if (enableBloom_) {
-        if (!pp->HasActiveMode(PostProcessMode::Bloom)) {
-            pp->AddActiveMode(PostProcessMode::Bloom);
-        }
-        auto& bloom = pp->GetBloomParams();
-        bloom.threshold = bloomThreshold_;
-        bloom.intensity = bloomIntensity_;
-        bloom.sigma = bloomSigma_;
-    } else {
-        pp->RemoveActiveMode(PostProcessMode::Bloom);
-    }
-
-    // 2. Color Grading (ToneMapping & HSV) - 色調補正系
-    // ※ 優先度（Priority）は PostProcessManager 側でハードコードされているため、
-    // ここでの AddActiveMode の順序は実行順序に影響しません。
-    if (enableColorGrading_) {
-        if (!pp->HasActiveMode(PostProcessMode::ToneMapping)) {
-            pp->AddActiveMode(PostProcessMode::ToneMapping);
-        }
-        if (!pp->HasActiveMode(PostProcessMode::HSV)) {
-            pp->AddActiveMode(PostProcessMode::HSV);
-        }
-        auto& tm = pp->GetToneMappingParams();
-        tm.exposure = exposure_;
-
-        auto& hsv = pp->GetHSVParams();
-        hsv.hue = hue_;
-        hsv.saturation = saturation_;
-        hsv.value = value_;
-    } else {
-        pp->RemoveActiveMode(PostProcessMode::ToneMapping);
-        pp->RemoveActiveMode(PostProcessMode::HSV);
-    }
-
-    // 3. Vignette - 画面演出系
-    if (enableVignette_) {
-        if (!pp->HasActiveMode(PostProcessMode::Vignette)) {
-            pp->AddActiveMode(PostProcessMode::Vignette);
-        }
-        auto& vignette = pp->GetVignetteParams();
-        vignette.radius = vignetteRadius_;
-        vignette.softness = vignetteSoftness_;
-    } else {
-        pp->RemoveActiveMode(PostProcessMode::Vignette);
+    for (auto& setting : overrides_) {
+        setting->ApplyToManager(pp);
     }
 }
 
 std::shared_ptr<Component> GlobalPostProcessComponent::Clone() {
     auto clone = std::make_shared<GlobalPostProcessComponent>();
-    clone->enableBloom_ = enableBloom_;
-    clone->bloomThreshold_ = bloomThreshold_;
-    clone->bloomIntensity_ = bloomIntensity_;
-    clone->bloomSigma_ = bloomSigma_;
-    clone->enableVignette_ = enableVignette_;
-    clone->vignetteRadius_ = vignetteRadius_;
-    clone->vignetteSoftness_ = vignetteSoftness_;
-    clone->enableColorGrading_ = enableColorGrading_;
-    clone->exposure_ = exposure_;
-    clone->hue_ = hue_;
-    clone->saturation_ = saturation_;
-    clone->value_ = value_;
+    for (const auto& setting : overrides_) {
+        clone->AddOverride(setting->Clone());
+    }
     return clone;
+}
+
+nlohmann::json GlobalPostProcessComponent::Serialize() {
+    nlohmann::json j = Component::Serialize();
+    nlohmann::json jOverrides = nlohmann::json::array();
+    
+    for (const auto& setting : overrides_) {
+        nlohmann::json jOverride = nlohmann::json::object();
+        setting->Serialize(jOverride);
+        jOverrides.push_back(jOverride);
+    }
+    
+    j["overrides"] = jOverrides;
+    return j;
+}
+
+void GlobalPostProcessComponent::Deserialize(const nlohmann::json& j) {
+    Component::Deserialize(j);
+    
+    overrides_.clear();
+    
+    if (j.contains("overrides") && j["overrides"].is_array()) {
+        for (const auto& jOverride : j["overrides"]) {
+            if (jOverride.contains("type")) {
+                std::string type = jOverride["type"].get<std::string>();
+                std::shared_ptr<IPostProcessSettings> setting = nullptr;
+                
+                if (type == "Bloom") {
+                    setting = PostProcessSettingsFactory::Create(PostProcessMode::Bloom);
+                } else if (type == "ColorGrading") {
+                    setting = PostProcessSettingsFactory::Create(PostProcessMode::ToneMapping);
+                } else if (type == "Vignette") {
+                    setting = PostProcessSettingsFactory::Create(PostProcessMode::Vignette);
+                }
+                // 新しいエフェクトはここに追記していく
+                
+                if (setting) {
+                    setting->Deserialize(jOverride);
+                    overrides_.push_back(setting);
+                }
+            }
+        }
+    }
 }
